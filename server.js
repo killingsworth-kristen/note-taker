5bf753e2da77cd44cdb94df88b240a781234eda3
Kristen <killingsworth_kristen@yahoo.com>
2022-11-01 23:08:59 -0400

    setting up file for heroku deployment

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const fs = require(`fs`);
 const db = require(`./db/db.json`)
 // instantiating express & creating port
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.static('public'));
